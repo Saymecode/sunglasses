069cab8aff0d963b23e085269e064ab5269d702c
Fedor Philippov <saymecode@saymecode.local>
2024-12-07 17:24:19 -0800

    mobile styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -209,6 +209,14 @@
                 });
             });
         }, 1000)
+
+        function updateVh() {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+
+        window.addEventListener('resize', updateVh);
+        updateVh();
     </script>
 
 
