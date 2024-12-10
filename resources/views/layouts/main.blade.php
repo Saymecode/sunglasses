5cd0314d7b2cfb88375bdabd933a89909015f7d9
Fedor Philippov <saymecode@saymecode.local>
2024-12-10 00:36:55 -0800

    improvements, slider

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,6 @@
 <html>
 <!DOCTYPE html>
-<link rel="stylesheet" href="./build/assets/app-CofMDCyq.css">
+<link rel="stylesheet" href="./build/assets/app-CzVZsQpT.css">
 <body lang="en">
 <head>
     <meta charset="UTF-8">
